1024a98bac7cd296ae57057189792f084967d755
kaylavu <kaylaavu@gmail.com>
2018-05-02 01:26:24 +0000

    Added ingredients

---
+++ ingredients.js
@@ -0,0 +1,15 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+var counter = 0;
+while (counter < ingredients.length){
+  console.log(ingredients[counter]);
+  counter++;
+}
+
+for (var i = 0; i < ingredients.length ; i++){
+  console.log(ingredients[i]);
+}
+
+for (var i = ingredients.length - 1; i >= 0 ; i--){
+  console.log(ingredients[i]);
+}
